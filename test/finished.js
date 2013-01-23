fbcbfce82e05fabf2f430acdb710060a5157ca06
James Halliday <mail@substack.net>
2013-01-22 18:41:06 -0800

    failing finished test

---
+++ test/finished.js
@@ -0,0 +1,43 @@
+var test = require('tape');
+var finished = require('../');
+var lines = [
+    'TAP version 13',
+    '# wait',
+    'ok 1 (unnamed assert)',
+    'not ok 2 should be equal',
+    '  ---',
+    '    operator: equal',
+    '    expected: 5',
+    '    actual:   4',
+    '  ...',
+    '',
+    '1..2',
+    '# tests 2',
+    '# pass  1',
+    '# fail  1'
+];
+
+test(function (t) {
+    t.plan(5);
+    var done = false;
+    
+    var stream = finished(function (results) {
+        t.equal(done, false);
+        
+        t.equal(results.pass.length, 1);
+        t.equal(results.pass[0].ok, true);
+        
+        t.equal(results.fail.length, 1);
+        t.equal(results.fail[1].ok, false);
+    });
+    
+    var iv = setInterval(function () {
+        if (lines.length === 0) {
+            clearInterval(iv);
+            done = true;
+        }
+        
+        var line = lines.shift();
+        stream.write(line + '\n');
+    }, 25);
+});
